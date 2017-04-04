2ba09e6b21f28450c322de12fd856544c2ff0ffa
Cameron Jones <cameronjonesweb@gmail.com>
2017-04-04 11:01:20 +1000

    Adding an implementation attribute to assist with debugging

--- facebook-page-feed-graph-api.php
+++ facebook-page-feed-graph-api.php
@@ -296,6 +296,7 @@ class cameronjonesweb_facebook_page_plugin {
 			'link' => true,
 			'linktext' => NULL,
 			'standard' => 'html5',
+			'_implementation' => 'shortcode'
 	    ), $filter );
 		if(isset($a['href']) && !empty($a['href'])){
 			$a['language'] = str_replace("-", "_", $a['language']);
@@ -303,7 +304,7 @@ class cameronjonesweb_facebook_page_plugin {
 			//Send the language as a parameter to the SDK
 			wp_localize_script( 'facebook-page-plugin-sdk', 'facebook_page_plugin_language', array( 'language' => $a['language'] ) );
 
-			$return .= '<div class="cameronjonesweb_facebook_page_plugin" data-version="' . CJW_FBPP_PLUGIN_VER . '" id="' . $this->facebook_page_plugin_generate_wrapper_id() . '">';
+			$return .= '<div class="cameronjonesweb_facebook_page_plugin" data-version="' . CJW_FBPP_PLUGIN_VER . '" data-implementation="' . esc_attr( $a['_implementation'] ) . '" id="' . $this->facebook_page_plugin_generate_wrapper_id() . '">';
 			$return .= '<div id="fb-root"></div><script>(function(d, s, id) {var js, fjs = d.getElementsByTagName(s)[0];if (d.getElementById(id)) return;js = d.createElement(s); js.id = id;js.src = "//connect.facebook.net/' . $a['language'] . '/sdk.js#xfbml=1&version=v2.5&appId=' . self::app_id() . '";fjs.parentNode.insertBefore(js, fjs);	}(document, \'script\', \'facebook-jssdk\'));</script>';
 			$return .= '<div class="fb-page" data-href="https://facebook.com/' . $a["href"] . '" ';
 			if(isset($a['width']) && !empty($a['width'])){
@@ -488,6 +489,7 @@ class cameronjonesweb_facebook_page_plugin_widget extends WP_Widget {
 			if( isset( $linktext ) && !empty( $linktext ) ){
 				$shortcode .= ' linktext="' . $linktext . '"';
 			}
+			$shortcode .= ' _implementation="widget"';
 			$shortcode .= ']';
 			echo do_shortcode( $shortcode );
 		}
