043b1d86fe38c57c552115c92c47aaaa76c9fba9
Cameron Jones <cameronjonesweb@gmail.com>
2017-04-11 19:38:41 +1000

    Updating metadata for 1.6.1

--- facebook-page-feed-graph-api.php
+++ facebook-page-feed-graph-api.php
@@ -3,7 +3,7 @@
  * Plugin Name: Facebook Page Plugin
  * Plugin URI: https://cameronjonesweb.com.au/wordpress-plugins/facebook-page-plugin/
  * Description: It's time to upgrade from your old like box! Display the Facebook Page Plugin from the Graph API using a shortcode or widget. Now available in 95 different languages
- * Version: 1.6.0
+ * Version: 1.6.1
  * Author: Cameron Jones
  * Author URI: https://cameronjonesweb.com.au
  * License: GPLv2
@@ -32,7 +32,7 @@ class cameronjonesweb_facebook_page_plugin {
 		define( 'CJW_FBPP_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 		define( 'CJW_FBPP_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 		define( 'CJW_FBPP_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
-		define( 'CJW_FBPP_PLUGIN_VER', '1.6.0' );
+		define( 'CJW_FBPP_PLUGIN_VER', '1.6.1' );
 		define( 'CJW_FBPP_PLUGIN_DONATE_LINK', 'https://www.patreon.com/cameronjonesweb' );
 		define( 'CJW_FBPP_PLUGIN_SURVEY_LINK', 'https://cameronjonesweb.typeform.com/to/BllbYm' );
 
